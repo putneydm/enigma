470b4ed07d390d45594b192dd84bb530b697f69d
David Putney <david@davidputney.com>
2018-05-14 20:09:24 -0400

    Reduces count of plugboard items to six.

--- source/scripts/to_concat/functions.js
+++ source/scripts/to_concat/functions.js
@@ -13,7 +13,7 @@ const initial = [{hello:"hello world"}]
 const numbersArr = newNumberArray(0, 26)
 const lettersArr = getLettersArr(numbersArr)
 const pivots = [20, 6, 17, 5, 15, 9]
-const plugs = newNumberArray(1, 13)
+const plugs = newNumberArray(1, 6)
 const plugboardArr = lettersData(lettersArr)
 const rotors = newNumberArray(1, 3).map((el, i) => {
   return { val: 0, id:i, p:pivots[i], sel:i<3?i:undefined }
