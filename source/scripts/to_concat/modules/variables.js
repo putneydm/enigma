d3e9b5c805f8c2f105958d78760844fc891c1454
David Putney <david@davidputney.com>
2018-06-24 22:34:03 -0400

    Reworks the data model for the plugboard array.

--- source/scripts/to_concat/modules/variables.js
+++ source/scripts/to_concat/modules/variables.js
@@ -5,6 +5,11 @@ const numbersArr = newNumberArray(0, 26)
 const lettersArr = getLettersArr(numbersArr)
 const pivots = [20, 6, 17, 5, 15, 9]
 const plugs = newNumberArray(1, 6)
+
+const plugsToo = plugs.map((el, i) => {
+  return {ccOne:null, ccTwo:null}
+})
+
 const plugboardArr = lettersData(lettersArr)
 const rotors = newNumberArray(1, 3).map((el, i) => {
   return { val: 0, id:i, p:pivots[i], sel:i<3?i:undefined, r:0 }
@@ -15,4 +20,4 @@ const rotorCount = newNumberArray(1, 6).map((el, i) => {
 const status = {keypress: undefined, result: undefined}
 const buttonStatus = {save:false, get:false, clear:false, dialog:false}
 
-export {initial, numbersArr, lettersArr, pivots, plugs, plugboardArr, rotors, rotorCount, status, buttonStatus}
+export {initial, numbersArr, lettersArr, pivots, plugs, plugboardArr, rotors, rotorCount, status, buttonStatus, plugsToo}
