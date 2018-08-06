41b7253d2714b014197f686188774ddff3a3fbf9
David Putney <david@davidputney.com>
2018-08-06 06:49:33 -0400

    Creates plugboard dropdown custom selector.

--- source/scripts/to_concat/modules/components/rotorringdrop.js
+++ source/scripts/to_concat/modules/components/rotorringdrop.js
@@ -2,6 +2,11 @@ import React, { Component } from "../../../../../node_modules/react"
 import ReactDOM from "../../../../../node_modules/react-dom"
 import { ListSelector } from "./listselector"
 
+// array that counts number of items to appear
+// rotorVal -- is default value of the dropdown
+// f is the function
+// id is the id of the ul item
+// rotorsarr is the values for each dropdown
 const RotorRingDrop=({ r, rotorVal, f, id, rotorsArr }) => {
   const clicky=(e) => {
     e.preventDefault()
@@ -31,4 +36,35 @@ const RotorRingDrop=({ r, rotorVal, f, id, rotorsArr }) => {
   </div>
   )
 }
-export { RotorRingDrop }
+
+const PlugBoardDrop=({ r, rotorVal, f, id, rotorsArr, selected, index }) => {
+  const clicky=(e) => {
+    e.preventDefault()
+    const test=e.target.classList.contains("disabled")
+    !test?f(id, index, e.target.value):f(id, rotorVal)
+  }
+  return (
+  <div
+    className="notes-picker-icn"
+  >
+    <ul
+      className="notes-colors"
+      id={id}
+      onClick={ clicky }
+    >
+      {rotorsArr.map((el, i) => {
+        return (
+          <ListSelector
+            key={i}
+            dispVal={el}
+            val={i}
+            disabledState={ rotorVal===i?"active":selected.some(ele => ele===i) && rotorVal !== i? "disabled": "inactive"}
+          />
+        )
+     })}
+    </ul>
+  </div>
+  )
+}
+
+export { RotorRingDrop, PlugBoardDrop }
