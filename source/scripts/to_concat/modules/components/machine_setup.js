6f712587190356969b64b5fe801e3810dd8cf639
putneydm <david@davidputney.com>
2018-09-03 20:31:44 -0400

    Refactors rotors so that each rotor has each of three selector components.

--- source/scripts/to_concat/modules/components/machine_setup.js
+++ source/scripts/to_concat/modules/components/machine_setup.js
@@ -7,16 +7,72 @@ import {RotorSelector} from "./rotor_selector"
 import {RingPositionSelector} from "./ring_position_selector"
 import {RotorPositionSelector} from "./rotor_position_selector"
 import {Plugboard} from "./plugboard"
+import { RotorRingDrop } from "./rotorringdrop"
+import { RingDrop } from "./RingDrop"
+import { Label, Header } from "./Label"
 
 const MachineSetup=({ rotors, rotorsArr, lettersArr, numbersArr, plugs, setRotorNumber, setRingPosition, setRotorPos, handleLetterboardArray, selected }) => {
   return (
     <div
+      className="machine-wrapper"
+    >
+    <div 
       className="rotors-wrapper"
     >
-      <div 
-        className="b-loose"
+      <div
+        className="rotor-labels-wrapper"
       >
-        <RotorSelector
+        <Label>Pick a rotor </Label>
+        <Label>Set locking ring</Label>  
+        <Label>Set rotor position</Label>
+      </div>      
+    { rotors.map((el, i) => {
+        return (
+            <div
+              className={ el.sel>=0?"rotor-selectors-wrapper active" :"rotor-selectors-wrapper" }
+            >
+            <Header>
+              {`Slot ${i + 1}`}
+            </Header>
+              {/* pick rotor */}
+            <RotorRingDrop
+              r={ rotorsArr }
+              rotorVal={ el.sel }
+              rotorsArr={ rotors } 
+              key={ i }
+              f={ setRotorNumber }
+              id={ i }
+              active={ true }
+              valSet={ el.sel>=0?true:false }
+            />
+            {/* set locking ring postion */}
+            <RingDrop
+              val={ el.r?el.r:0 }
+              r={ numbersArr }
+              key={ i+100 }
+              id={i}
+              f={ setRingPosition }
+              active={ el.active }
+              valSet={ el.r?true:false } 
+              />
+              {/* set rotor value */}
+            <RingDrop
+              val={ el.val?el.val:0 }
+              r={ lettersArr }
+              key= {i + 200 }
+              id={ i }
+              f={ setRotorPos }
+              active={ el.active }
+              valSet={ el.val>=0?true:false }
+            />
+            </div>
+        )
+      }) }
+    </div>
+      {/* <div 
+        className="b-loose"
+      > */}
+        {/* <RotorSelector
           rotors={ rotors }
           r={ rotorsArr }
           f={ setRotorNumber }
@@ -30,8 +86,8 @@ const MachineSetup=({ rotors, rotorsArr, lettersArr, numbersArr, plugs, setRotor
           count={ rotors }
           r={ lettersArr }
           f={ setRotorPos }
-        />
-      </div>
+        /> */}
+      {/* </div> */}
       <Plugboard
         count = { lettersArr }
         plugs = { plugs }
--- source/scripts/to_concat/modules/components/rotor_selector.js
+++ source/scripts/to_concat/modules/components/rotor_selector.js
@@ -3,15 +3,30 @@ import ReactDOM from "../../../../../node_modules/react-dom"
 import { RotorRingDrop } from "./rotorringdrop"
 import { Label } from "./label"
 // componet for selecting which rotor -- 1 through six -- to use
-const RotorSelector=({ rotors, r, f }) => {
+const RotorSelector=({ el, i, rotors, r, f }) => {
+  console.log(el)
   return (
     <div
       className="rotor-selector b-loose"
     >
-      <Label>
+
+    <RotorRingDrop
+        r={ r }
+        rotorVal={ el.sel }
+        rotorsArr={ rotors }
+        key={ i }
+        f={ f }
+        id={ i }
+        active={ true }
+        valSet={ el.sel>=0?true:false }
+      />
+
+
+
+      {/* <Label>
         Pick a rotor
-      </Label>
-      {rotors.map((el, i) => {
+      </Label> */}
+      {/* {rotors.map((el, i) => {
         return (
           <RotorRingDrop
             r={ r }
@@ -24,7 +39,7 @@ const RotorSelector=({ rotors, r, f }) => {
             valSet={ el.sel>=0?true:false }
           />
         )
-      })}
+      })} */}
     </div>
   )
 }
