15d97abefde4cbcf35251a637866d68362ebf089
David Putney <david@davidputney.com>
2018-09-22 10:53:48 -0400

    Deletes unneeded code.

--- source/scripts/to_concat/modules/components/plugboard.js
+++ source/scripts/to_concat/modules/components/plugboard.js
@@ -3,75 +3,7 @@ import ReactDOM from "react-dom"
 import { PlugBoardDrop } from "./rotorringdrop"
 import { Label } from "./label"
 
-// const RotorRingDrop=({ r, rotorVal, f, id, rotorsArr }) => {
-//   const clicky=(e) => {
-//     e.preventDefault()
-//     const test=e.target.classList.contains("disabled")
-//     !test?f(id, e.target.value):f(id, rotorVal)
-//  }
-//   return (
-//   <div
-//     className="notes-picker-icn"
-//   >
-//     <ul
-//       className="notes-colors"
-//       id={id}
-//       onClick={ clicky }
-//     >
-//       {r.map((el, i) => {
-//         return (
-//           <li
-//               key={i}
-//               value={i}
-//               // onClick={ clicky }
-//               className={ rotorVal===i?"active":rotorsArr.some(ele => ele.sel===i) && rotorVal !== i? "disabled": "inactive"}
-//             >
-//               {`${el}`}
-//           </li>
-//         )
-//      })}
-//     </ul>
-//   </div>
-//   )
-// }
-//
-// const PlugboardDropdown=({ r, id, f, item, index, selected, def }) => {
-//     const clicky=(e) => {
-//       e.preventDefault()
-//       f(item, index, e.target.value)
-//     }
-//     return (
-//       <select
-//         onChange={ clicky }
-//         id={ id }
-//         value= { def !== null?def:"def" }
-//       >
-//         <option
-//           disabled="true"
-//           hidden="true"
-//           value="def"
-//         >
-//           Choose letter
-//         </option>
-//         {r.map((el, i) => {
-//           return (
-//             <option
-//               key={ i }
-//               value={ i }
-//               disabled={ selected.some((le) => i === le && i !== def) }
-//             >
-//               {`${el}`}
-//             </option>
-//           )
-//         })}
-//       </select>
-//     )
-//   }
-
-
 const Plugboard=({ plugs, count, f, selected, animate=false }) => {
-  // console.log("selected", selected);
-  // console.log("plugs", plugs);
   return (
     <div
       className="plugboard-container"
